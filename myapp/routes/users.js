e069a6d0bb99710b8f3a45cf22d9ec7a5187ec99
vask14 <vasksv97@gmail.com>
2018-01-23 22:44:00 +0200

    Crud operations using ajax

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,43 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const router = express.Router();
-const path = require('path');
-const fs = require('fs');
+var express = require('express');
+var router = express.Router();
 let users = require('../data/user.json');
 
-//GET
-router.get('/',function(req,res) {
-  res.render('user',{users:users});
+
+var currentId = 2;
+router.get('/',function(req,res,next) {
+  res.render('user');
 });
 
-router.post('/user',function(req,res,next) {
-  res.redirect('/users');
-  users.push(req.body);
+
+/* GET users listing. */
+router.get('/user', function(req, res, next) {
+  res.send({users:users});
 });
 
-//DELETE
-router.post('/user/:id',function(req,res) {
-  res.redirect('/users');
-  let id = req.params.id;
-  let newUser = users.find(function(item) {
-    return item.id == req.params.id;
+
+router.post('/user', function(req, res) {
+  var userName = req.body.name;
+  let userSurname = req.body.surname;
+  let userAge = req.body.age;
+  let userMail = req.body.mail
+  currentId++;
+
+  users.push({
+      id: currentId,
+      name: userName,
+      surname: userSurname,
+      age:userAge,
+      mail:userMail
   });
-  let index = users.indexOf(newUser);
-  users.splice(index,1,req.body);
+
+  res.send('Successfully created product!');
 });
 
+router.put('/user/:id', function(req, res) {
+  var id = req.params.id;
+  var newName = req.body.newName;
+  var newSurname = req.body.newSurname;
+  var newAge = req.body.newAge;
+  var newMail = req.body.newMail;
+  
+  var found = false;
 
-//PUT
-router.post('/user/delete/:id',function(req,res) {
-  res.redirect('/users'); 
-  let id = req.params.id;
-  console.log(req.body);
-  users.forEach(function(user,index) {
-    if(user.id == id) {
-      users.splice(index,1);
-    }
+  users.forEach(function(user, index) {
+      if (!found && user.id === Number(id)) {
+          user.name = newName;
+          user.surname = newSurname;
+          user.age = newAge;
+          user.mail = newMail;
+      }
   });
+  res.send('Successfully updated');
+});
+
+router.delete('/user/:id', function(req, res) {
+  var id = req.params.id;
+
+  var found = false;
+  
+  users.forEach(function(user, index) {
+      if (!found && user.id === Number(id)) {
+          users.splice(index, 1);
+      }
+  });
+
+  res.send('Successfully deleted');
 });
 
 module.exports = router;
+
